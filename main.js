6a795dbabcdf31f9ff0ce812741a4e09d8b0232d
Kozachenko Volodymyr <kz4nkovova@gmail.com>
2024-10-18 21:44:43 +0200

    Loading the task

---
+++ main.js
@@ -0,0 +1,16 @@
+let undefinedCheck;
+let nullCheck = null;
+let booleanCheck = true;
+let numberCheck = 1;
+let stringCheck = "Hello world";
+let objectCheck = {};
+let symbolCheck = Symbol('symbolCheck')
+let bigIntCheck = BigInt(5)
+console.log(typeof(undefinedCheck))
+console.log(typeof(nullCheck) + " " + typeof(null))
+console.log(typeof(booleanCheck))
+console.log(typeof(numberCheck))
+console.log(typeof(stringCheck))
+console.log(typeof(objectCheck))
+console.log(typeof(symbolCheck))
+console.log(typeof(bigIntCheck))
