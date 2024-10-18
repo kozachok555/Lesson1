10aa451f524d8534f99f2e14eb8c2ce40ecc80d5
Kozachenko Volodymyr <kz4nkovova@gmail.com>
2024-10-18 22:28:05 +0200

    Done the task

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Task1
 let undefinedCheck;
 let nullCheck = null;
 let booleanCheck = true;
@@ -14,3 +15,16 @@ console.log(typeof(stringCheck))
 console.log(typeof(objectCheck))
 console.log(typeof(symbolCheck))
 console.log(typeof(bigIntCheck))
+
+//Task2
+const message1 = prompt("Write your message");
+const message2 = prompt("Write your message");
+const message3 = prompt("Write your message");
+console.log(`${message1}
+    ${message2}
+    ${message3}`)
+
+//Task3
+const number = prompt("Enter the number which consist 5 figures")
+let changedNumber = number.toString().split('').join(' ');
+console.log(changedNumber)
