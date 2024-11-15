016deeb871b9270426e70ef269dd3fae4dd53a8d
Kozachenko Volodymyr <kz4nkovova@gmail.com>
2024-11-15 20:06:21 +0100

    CW-11

---
+++ main.js
@@ -0,0 +1,36 @@
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<CW - 11>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
+
+                //Task - 1//
+
+// const ulClassFind = document.querySelector(".list")
+// const secondLiFind = document.querySelectorAll("li")
+// secondLiFind[1].textContent = "Any Text";
+// const divFind = document.querySelector("div")
+// divFind.style.color = 'black'
+
+                //Task - 2//
+    
+// const inputText = document.querySelector("input")
+// const btnFind = document.getElementById('btn')
+// btnFind.addEventListener("click", function(){
+//     console.log(inputText.value)
+// })
+
+                //Task - 3//
+
+
+// const findBtn = document.getElementById("btn")
+// const divBoxElements = document.getElementById("box")
+// divBoxElements.style.display = "none"
+// findBtn.addEventListener('click', function(){
+//     if(divBoxElements.style.display === "none"){
+//         divBoxElements.style.display = "block"
+//         const createNewElement = document.createElement("li")
+//         createNewElement.textContent = "Нове посилання"
+//         divBoxElements.appendChild(createNewElement)
+//     }else if(divBoxElements.style.display === "block"){
+//         divBoxElements.style.display = "none"
+//     }
+// })
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<END-CW - 11>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>//
